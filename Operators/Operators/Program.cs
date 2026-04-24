5871558b4dc37335f3e1fbf095e8123dec915a0b
DESKTOP-N5GFK26\Daniel <GameDeveloperUE67@gmail.com>
2026-04-24 18:30:35 +0200

    Course doodles with operators and string builder

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,104 @@
+﻿byte a = 100;
+byte b = 200;
+
+var c = a + b;
+//Console.WriteLine($"Max value of byte: {byte.MaxValue}");
+//Console.WriteLine($"Min value of byte: {byte.MinValue}");
+//Console.WriteLine(c);
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+/*
+checked
+{
+    //a = (byte)(a + b);
+    a += b;
+}
+*/
+/*
+a = (byte)((sbyte)a + (sbyte)b);
+Console.WriteLine(a);
+
+int d = 10;
+int e = 0;
+int result;
+
+try
+{
+    result = d / e;
+    Console.WriteLine(result);
+}
+catch(DivideByZeroException)
+{
+    Console.WriteLine("Divided by zero exception");
+}
+result = 0;
+Console.WriteLine(result);
+*/
+
+Console.WriteLine("Write first number: ");
+int val1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write second number: ");
+int val2 = int.Parse(Console.ReadLine());
+int result = val1 + val2;
+Console.WriteLine($"{val1} + val2 = {result}");
+result = val1 - val2;
+Console.WriteLine($"{val1} - {val2} = {result}");
+result = val1 * val2;
+Console.WriteLine($"{val1} * {val2} = {result}");
+result = val1 / val2;
+Console.WriteLine($"{val1} / {val2} = {result}");
+result = val1 % val2;
+Console.WriteLine($"{val1} % {val2} = {result}");
+
+//Type casting: converting one type of data into another type of data.
+
+//Implicit typecasting
+int x = 10;
+double y = x;
+
+
+//explicit typecasting
+double d = 10.5;
+int e = (int)d;
+Console.WriteLine(e);
+
+double g = 10.50008;
+int h = Convert.ToInt16(g);
+Console.WriteLine(h);
+
+
+char f = 'A';
+int i = (int)f;
+Console.WriteLine(i);
+string mystring = "hello";
+/*
+foreach(char car in mystring)
+{
+    Console.WriteLine(car);
+}
+*/
+/*
+for (i = 0; i < mystring.Length; i++)
+{
+
+    //Console.WriteLine(mystring.Length);
+    Console.WriteLine(mystring[i]);
+    int sum1 = (int)mystring[i];
+    Console.WriteLine(sum1);
+}
+
+*/
+
+Console.WriteLine("Write nominator: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write denominator: ");
+int n2 = int.Parse(Console.ReadLine());
+double divisition = (double)n1/n2;
+Console.WriteLine($"Division result: {n1} / {n2} = {divisition}");
+
+
+
+
+Console.ReadLine();
---
+++ StringConcatenationExample/StringConcatenationExample/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace StringConcatenationExample
+{
+    internal class StringConcatenationExample
+    {
+        static void Main(string[] args)
+        {
+            const int sLen = 30, Loops = 5000;
+            int i;
+            string sSource = new String('X', sLen);
+            string sDest = "";
+
+            // Time string concatenation.
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            for (i = 0; i < Loops; i++) sDest += sSource;
+            stopwatch.Stop();
+            Console.WriteLine($"Concatenation took {stopwatch.ElapsedMilliseconds} ms.");
+
+            // Time StringBuilder.
+            stopwatch.Restart();
+            System.Text.StringBuilder sb = new System.Text.StringBuilder((int)(sLen * Loops * 1.1));
+            for (i = 0; i < Loops; i++)
+                sb.AppendLine(sSource);
+                //Console.WriteLine(sb.ToString());
+            sDest = sb.ToString();
+            Console.WriteLine(sDest);
+            stopwatch.Stop();
+            Console.WriteLine($"String Builder took {stopwatch.ElapsedMilliseconds} ms.");
+
+            //Time string interpolation
+
+            stopwatch.Restart();
+            for (i = 0; i < Loops; i++) sDest += $"{sSource}";
+            stopwatch.Stop();
+            Console.WriteLine($"String interpolation took {stopwatch.ElapsedMilliseconds} ms");
+
+            // Make the console window stay open
+            // so that you can see the results when running from the IDE.
+            Console.WriteLine();
+            Console.Write("Press Enter to finish ... ");
+            Console.Read();
+        }
+    }
+}
